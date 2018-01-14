3530bed0f41d6e66ba61a8b090a001df1aa2adc2
Carlos Moreno <tulsadesigner@gmail.com>
2018-01-13 21:07:25 -0600

    programming historian - python lessons

---
+++ obo.py
@@ -0,0 +1,8 @@
+# obo.py
+
+def stripTags(pageContents):
+    startLoc = pageContents.find("<p>")
+    endLoc = pageContents.rfind("<br/>")
+
+    pageContents = pageContents[startLoc:endLoc]
+    return pageContents
